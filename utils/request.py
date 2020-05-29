8e56809cdfcf36a2b618e0d7e5bc688330643df2
Keletso M <u14062552@tuks.co.za>
2020-05-29 16:20:44 +0200

    a

--- utils/request.py
+++ utils/request.py
@@ -26,8 +26,8 @@ import numpy as np
 # We prepare a DataFrame with the public test set + riders data
 # from the Zindi challenge.
 test = pd.read_csv('data/test_data.csv')
-#riders = pd.read_csv('data/riders.csv')
-#test = test.merge(riders, how='left', on='Rider Id')
+riders = pd.read_csv('data/riders.csv')
+test = test.merge(riders, how='left', on='Rider Id')
 
 # Convert our DataFrame to a JSON string.
 # This step is necessary in order to transmit our data via HTTP/S
@@ -47,10 +47,10 @@ print(f"Querying API with the following data: \n {test.iloc[1].to_list()}")
 print("")
 # Here `api_response` represents the response we get from our API
 api_response = requests.post(url, json=feature_vector_json)
-
+print(api_response)
 # Display the prediction result
 print("Received POST response:")
 print("*"*50)
-print(f"API prediction result: {api_response.json()[0]}")
+print(f"API prediction result: {api_response.json()}")
 print(f"The response took: {api_response.elapsed.total_seconds()} seconds")
 print("*"*50)
